f0a38888a23cec8508c8827ea150262493b0cd3e
Francisco Franco <franciscofranco.1990@gmail.com>
2019-01-18 18:05:32 +0000

    zram: fix my own derp. Shouldn't fix merge conflicts at 6am
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1314,7 +1314,8 @@ static int zram_add(void)
 	ret = sysfs_create_group(&disk_to_dev(zram->disk)->kobj,
 				&zram_disk_attr_group);
 	if (ret < 0) {
-		pr_warn("Error creating sysfs group");
+		pr_err("Error creating sysfs group for device %d\n",
+				device_id);
 		goto out_free_disk;
 	}
 	strlcpy(zram->compressor, default_compressor, sizeof(zram->compressor));
